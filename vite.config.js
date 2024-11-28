3595440cf9549258d4f437660a1b869945915e1a
Latidude Maps <latidude.maps@gmail.com>
2024-11-28 10:05:32 +0100

    -

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,13 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/GeologiaVDA/',  // Assicurati che questo corrisponda esattamente al nome del repository
+  base: '/GeologiaVDA/', // Replace with your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: [
-            'react',
-            'react-dom',
-            'maplibre-gl',
-            'pmtiles',
-            '@geomatico/maplibre-cog-protocol',
-            '@turf/turf',
-            'recharts'
-          ],
-          controls: [
-            './LayerControl.js',
-            './MapInfo.js',
-            './GeoInfoPanel.js',
-            './MinimapControls.js',
-            './TerrainControls.js',
-            './SettingsControl.js',
-            './CopyrightControl.js',
-            './ProfileControl.jsx'
-          ]
-        },
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          return `assets/${info[0]}-[hash:8].${ext}`;
-        }
+        manualChunks: undefined
       }
     }
   }
